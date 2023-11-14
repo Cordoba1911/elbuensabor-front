2cb174b4a2b3be39548f1ee283b674463c0b02e6
Facundo Yanzon <facundo.yanzon@colsa.com.ar>
2023-11-14 07:55:02 -0300

    Aca esta el tema de las peticiones con la libreria, ejemplo de get y post

---
+++ src/axiosService.tsx
@@ -0,0 +1,25 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+const customAxiosInstance = axios.create({
+  baseURL: ACA SE PONE LA URL BASE,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+customAxiosInstance.interceptors.request.use(
+  (axiosConfigRequest: AxiosRequestConfig) => {
+    return axiosConfigRequest;
+  }
+);
+customAxiosInstance.interceptors.response.use(
+  (axiosResponse): any => axiosResponse,
+  (error): any => {
+    if (error.code === 'ERR_NETWORK') {
+      return Promise.reject({ Message: 'Ocurrió un error de red' });
+    }
+    return Promise.reject(error);
+  }
+);
+
+export { customAxiosInstance };
